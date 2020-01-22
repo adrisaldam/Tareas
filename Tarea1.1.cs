17bc8d666aa849e556351999c7f5ead16aca87ff
adrisaldam <60115506+adrisaldam@users.noreply.github.com>
2020-01-21 20:57:17 -0500

    Add files via upload

---
+++ Tarea1.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Hola, ingresa el valor de 'Y' y 'Z'");
+            Console.WriteLine("Valor de 'Y': ");
+            double valorZ = double.Parse(Console.ReadLine());
+            Console.WriteLine("Valor de 'Z': ");
+            double valorY = double.Parse(Console.ReadLine());
+
+            //Cálculo de datos faltantes
+            double valorT = Math.Sqrt((valorZ) * (valorZ) + (valorY) * (valorY));
+            Console.WriteLine("El valor de 'T' es: " + valorT);
+
+            double anguloC1 = Math.Asin(valorY / valorT);
+            double anguloC = anguloC1 * 180 / Math.PI; 
+            Console.WriteLine("El valor del ángulo 'C' es: " + anguloC);
+
+            double anguloA1 = Math.Asin(valorZ / valorT);
+            double anguloA = anguloA1 * 180 / Math.PI;
+            Console.WriteLine("El valor del ángulo 'A' es: " + anguloA);
+            
+        }
+    }
+}
---
+++ Tarea1.2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tarea_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Hola, ingresa el valor de 'T' y el ángulo 'A'. " + 
+                "Recuerde escribir los datos decimales con coma.");
+            Console.WriteLine("Valor de 'T': ");
+            double valorT = double.Parse(Console.ReadLine());
+            Console.WriteLine("Valor de 'A': ");
+            double anguloA1 = double.Parse(Console.ReadLine());
+            double anguloA = anguloA1 * (Math.PI / 180);
+
+            //Cálculo de datos faltantes
+            double ladoY = Math.Round(Math.Sin(anguloA) * valorT);
+            Console.WriteLine("El valor del lado 'Y' es: " + ladoY);
+
+            double ladoZ = Math.Round(Math.Cos(anguloA) * valorT);
+            Console.WriteLine("El valor del lado 'Z' es: " + ladoZ);
+
+            double anguloC1 = Math.Asin(ladoZ / valorT);
+            double anguloC = anguloC1 * 180 / Math.PI;
+            Console.WriteLine("El valor del ángulo 'C' es: " + anguloC);
+        }
+    }
+}

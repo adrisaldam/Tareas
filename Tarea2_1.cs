d89b7c6191d69b7d25e6cede5c607a058f977415
adrisaldam <60115506+adrisaldam@users.noreply.github.com>
2020-01-29 19:56:38 -0500

    Add files via upload

---
+++ Tarea2_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique su salario mensual exacto");
+            int salario = int.Parse(Console.ReadLine());
+
+            if (salario < 1755606)
+                Console.WriteLine("Debe pagar la Tarifa A");
+
+            else if (salario >= 1755606 && salario < 3511212)
+                Console.WriteLine("Debe pagar la tarifa B");
+
+            else
+                Console.WriteLine("Debe pagar la tarifa C");
+
+        }
+    }
+}
